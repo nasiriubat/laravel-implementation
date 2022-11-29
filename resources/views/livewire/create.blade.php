8dc02878a0305f81b06b5734955daac78a94f3e1
nasiriubat <shuvo131662@gmail.com>
2022-11-29 15:31:33 +0600

    done

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,16 @@
+<div class="d-flex justify-content-end">
+<button wire:click="index()" class="btn btn-primary btn-sm">Back</button>
+</div>
+<form>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Title:</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Title" wire:model="title">
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlInput2">Body:</label>
+        <textarea type="email" class="form-control" id="exampleFormControlInput2" wire:model="body" placeholder="Enter Body"></textarea>
+        @error('body') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <button wire:click.prevent="store()" class="btn btn-success mt-2">Save</button>
+</form>
